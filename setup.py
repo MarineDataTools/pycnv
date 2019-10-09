d6be6d518d9e11947ed823bc9a7ece4c6984beca
Peter Holtermann <peter.holtermann@io-warnemuende.de>
2019-10-09 10:54:28 +0200

    moved gui to pyctd

--- setup.py
+++ setup.py
@@ -7,17 +7,16 @@ with open(os.path.join(ROOT_DIR, 'VERSION')) as version_file:
 
 setup(name='pycnv',
       version=version,
-      description='Tools to convert Seabird cnv textfiles',
+      description='Tool to convert Seabird cnv textfiles',
       url='https://github.com/MarineDataTools/pycnv',
       author='Peter Holtermann',
       author_email='peter.holtermann@io-warnemuende.de',
       license='GPLv03',
       packages=['pycnv'],
       scripts = [],
-      entry_points={ 'console_scripts': ['pycnv_cmd=pycnv.pycnv:main', 'pycnv_sum_folder=pycnv.pycnv_sum_folder:main', 'pycnv=pycnv.gui.pycnv_gui:main']},
+      entry_points={ 'console_scripts': ['pycnv=pycnv.pycnv:main', 'pycnv_sum_folder=pycnv.pycnv_sum_folder:main']},
       package_data = {'':['VERSION','stations/iow_stations.yaml','rules/standard_names.yaml']},
       install_requires=[ 'gsw', 'pyproj','pytz','pyaml' ],
       zip_safe=False)
 
 
-# TODO Depends on gsw, pyproj, pytz
